790a41fdaa39a828c40555a3772237de1e827a2a
Grichu <Grichu>
2018-01-29 21:16:59 -0300

    testeo 3

--- enviar.php
+++ enviar.php
@@ -7,7 +7,7 @@ $subject = "envio mail"
 $message = $_REQUEST['message']
 $from = "Enviado desde la web"
 
-echo ($recipient, $subject, $message, $email);
+echo ($recipient . $subject . $message . $email);
 //mail($recipient, $subject, $message, $from);
 
 //mail(to,subject,message,headers,parameters);
